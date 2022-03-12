0fd3eb366e5e6b6396f70d907dbfcfcbb5b55522
Thomas <78752122+T-Jakubowski@users.noreply.github.com>
2022-03-12 21:35:31 +0100

    first commit

---
+++ DAOStock.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Gestion_Stock_PPE
+{
+    internal class DAOStock
+    {
+        private MySqlConnection con;
+
+        public DAOStock()
+        {
+            con = new MySqlConnection("server=localhost;userid=root;password=;database=stocks");
+        }
+        public void getObject()
+        {
+            con.Open();
+            con.Close();
+        }
+        public void deleteObject(string pharmacieCode, string objetCode)
+        {
+            con.Open();
+            string query = "delete from stocks WHERE pharmacieCode = '" + pharmacieCode + "' and objetCode = '" + objetCode + "';";
+            MySqlCommand delete = new MySqlCommand(query, con);
+            delete.ExecuteNonQuery();
+            con.Close();
+        }
+        public void updateObjectByName(int quantite, string pharmacieCode, string objetCode)
+        {
+            con.Open();
+            string query = "UPDATE stocks SET quantite = " + quantite +" WHERE pharmacieCode = '" + pharmacieCode +  "' and objetCode = '" + objetCode + "';";
+            MySqlCommand update = new MySqlCommand(query, con);
+            update.ExecuteNonQuery();
+            con.Close();
+        }
+        public void insertObject(int quantite, string pharmacieCode, string objetCode)
+        {
+            con.Open();
+            string query = "INSERT INTO stocks (pharmacieCode, objetCode, quantite) VALUES('" + pharmacieCode + "', '" + objetCode + "', " + quantite + "); ";
+            MySqlCommand insert = new MySqlCommand(query, con);
+            insert.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows;
+
+namespace Gestion_Stock_PPE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //string cs = "sever=localhost;userid=root;password=;database=stocks";
+            DAOStock dAOStock = new DAOStock();
+            try
+            {
+                dAOStock.updateObjectByName(4, "AR719", "AX938");
+                //dAOStock.insertObject(5, "DF066", "VP349");
+                dAOStock.deleteObject("DF066", "VP349");
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
